44110707d14687350118ec49968920553dfd107a
MrNoForks <testingiosapp12345@gmail.com>
2019-05-06 17:50:51 +0530

    Added timer to spawn aliens at intervals alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)      // creates a physics body alien.physicsBody?.categoryBitMask       // A mask that defines which categories this physics body belongs to. alien.physicsBody?.contactTestBitMask       //which categories of physics bodies cause intersection notifications with this physics body alien.physicsBody?.collisionBitMask = 0     //  A mask that defines which categories of physics bodies can collide with this physics body.

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -22,6 +22,17 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         }
     }
     
+    private weak var gameTimer : Timer!
+    
+    private var possibleAliens = ["alien","alien2","alien3"]
+    
+    //bitwise left shift modifier to give each category a unique identifier to detect collision
+    let alienCategory : UInt32 = 0x1 << 1
+    //let alienCategory : UInt32 = 2
+    
+    let photonTorpedoCategory : UInt32 = 0x1 << 0
+    //let photonTorpedoCategory : UInt32 = 1
+    
     override func didMove(to view: SKView) {
         
         starField = SKEmitterNode(fileNamed: "Starfield")
@@ -59,8 +70,48 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         
         self.addChild(scoreLabel)
         
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
+        
     }
     
+    
+    @objc func addAlien(){
+        
+        //possibleAliens.shuffle()
+        
+       // Now when we add a new alien whole of array gets shuffled using GameplayKit
+        possibleAliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as! [String]
+
+        let alien = SKSpriteNode(imageNamed: possibleAliens[0])
+        
+        
+      //  let position =  arc4random_uniform(UInt32(size.height/2))
+        
+        // Now when we select a random position for aliens using GameplayKit
+        let randomAlienPosition = GKRandomDistribution(lowestValue: -(Int(size.height/2)), highestValue: Int(size.height/2))
+        
+        let position = CGFloat(randomAlienPosition.nextInt())
+        
+        alien.position = CGPoint(x: position, y: (size.height/2) - alien.size.height)
+        
+        
+        // To add a physicsbody to alien
+        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+        
+        alien.physicsBody?.isDynamic = true
+        
+        //Now we need Category bitmask,Contact test bitmask and a collision bitmask
+        //Those are needed to actually calculate when are we hitting alien with an torpedo
+        
+        alien.physicsBody?.categoryBitMask = alienCategory
+        alien.physicsBody?.contactTestBitMask = photonTorpedoCategory
+        alien.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(alien)
+        
+
+    }
 
     
     override func update(_ currentTime: TimeInterval) {
