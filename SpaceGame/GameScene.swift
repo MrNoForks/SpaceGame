216bf67668ea727854c8051c50f954fe924ce844
MrNoForks <testingiosapp12345@gmail.com>
2019-05-06 15:28:13 +0530

    self.physicsWorld.gravity for gravity for physics bodies in physicsWorld self.physicsWorld.contactDelegate = self called when  two physics bodies come in contact with each other. Added SKLabelNode iosfonts.com for all fonts in iOS

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -9,10 +9,18 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene , SKPhysicsContactDelegate {
     
     var starField : SKEmitterNode!
     var player:SKSpriteNode!
+    
+    
+    var scoreLabel : SKLabelNode!
+    var score : Int = 0 {
+        didSet{
+            scoreLabel.text = "Score : \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         
@@ -34,6 +42,23 @@ class GameScene: SKScene {
         player.position = CGPoint(x: 0, y: -(size.height/2) + (player.size.height + 20))
         
         self.addChild(player)
+        
+       // A vector that specifies the gravitational acceleration applied to physics bodies in the physics world
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
+        //A delegate that is called when two physics bodies come in contact with each other.
+        self.physicsWorld.contactDelegate = self
+        
+        
+        scoreLabel = SKLabelNode(text: "Score : 0")
+        
+        scoreLabel.position = CGPoint(x : 0,y : (size.height/2) - 100)
+        scoreLabel.fontName = "AppleSDGothicNeo-Medium"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        self.addChild(scoreLabel)
+        
     }
     
 
